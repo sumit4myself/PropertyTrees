76e1860fea74d935af4904f7c75132b6de813da0
Happy Dhingra <happy.dhingra@gmail.com>
2018-02-05 18:41:15 +0530

    Updated

--- propertytrees-ui/propertytrees-ui-frontend/angularjs-2/src/app/home/home.component.ts
+++ propertytrees-ui/propertytrees-ui-frontend/angularjs-2/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SearchFilter} from '../common/search.filter';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchFilter: SearchFilter) { }
 
   ngOnInit() {
   }
