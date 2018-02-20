dea0f20452b573b78efa1fff43598fdedcb3aaf3
shiv <shiv@DESKTOP-0GTF2FV>
2018-02-20 11:59:42 +0530

    Resolved issue for user registration

--- propertytrees-common/src/main/java/com/propertiestree/common/entity/User.java
+++ propertytrees-common/src/main/java/com/propertiestree/common/entity/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 
 	private static final long serialVersionUID = -468285446816535561L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@Column(unique = true, nullable = false, updatable = false)
 	private String uuid;
